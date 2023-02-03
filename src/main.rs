bbae2af834173f479213589419091d345f47758d
ChrisOtt0 <sophusott@gmail.com>
2023-02-03 23:37:35 +0100

    Test1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use rppal::gpio::{Gpio, InputPin, OutputPin};
+
 fn main() {
-    println!("Hello, world!");
+    let gpio = Gpio::new()
+        .unwrap_or_else(|e| panic!("Failed to create struct Gpio: {e}"));
+
+    let input: InputPin = gpio.get(18)
+        .unwrap_or_else(|e| panic!("Failed to create struct Pin: {e}"))
+        .into_input_pulldown();
+
+    let mut output: OutputPin = gpio.get(26)
+        .unwrap_or_else(|e| panic!("Failed to create struct Pin: {e}"))
+        .into_output();
+
+    let mut count = 0;
+
+    loop {
+        if count > 200 {
+            break;
+        }
+
+        let state = input.is_low();
+
+        if state {
+            count += 1;
+            output.set_high();
+        } else {
+            count = 0;
+            output.set_low();
+        }
+    };
+
+    output.set_low();
 }
